3564dff23f75863b7bd21c250caffd22ff43b795
raaz-srijan <srijannospace@gmail.com>
2026-06-01 15:57:26 +0000

    added middleware and reformed routes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
-import 'dotenv/config';
-import express from 'express';
-import { connectDb } from './infrastructure/connectDb';
-import { ENV } from './infrastructure/env';
+import "dotenv/config";
+import express from "express";
+import cookieParser from "cookie-parser";
+
+// Infrastructure
+import { connectDb } from "./infrastructure/connectDb";
+import { ENV } from "./infrastructure/env";
+
+// Routes
+import permissionRoute from "./modules/permission/permission.route";
+import roleRoute from "./modules/role/role.route";
+import authRoute from "./modules/auth/auth.route";
+import userRoute from "./modules/user/user.route";
+
+// Error Handler
+import globalError from "./shared/error/globalError";
 
 const app = express();
-
-app.use(express.json());
-
 const PORT = ENV.PORT;
 
-import permissionRoute from "./modules/permission/permission.route";
+// MIDDLEWARE
 
-app.use("api/v1/permissions", permissionRoute );
+app.use(express.json());
+app.use(cookieParser());
+
+// ROUTES
+
+app.use("/api/v1/permissions", permissionRoute);
+app.use("/api/v1/roles", roleRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+
+//GLOBAL ERROR HANDLER
+
+app.use(globalError);
+
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
     connectDb();
-})
+});
