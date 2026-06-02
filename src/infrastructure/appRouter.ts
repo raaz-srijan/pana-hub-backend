1aee96cce3bd2d56d2df46c4b426e29197fc4706
raaz-srijan <srijannospace@gmail.com>
2026-06-02 11:47:11 +0000

    added routes

--- src/infrastructure/appRouter.ts
+++ src/infrastructure/appRouter.ts
@@ -5,6 +5,8 @@ import authRoute from "../modules/auth/auth.route.js";
 import vendorRoute from "../modules/vendor/vendor.route.js";
 import permissionRoute from "../modules/permission/permission.route.js";
 import roleRoute from "../modules/role/role.route.js";
+import categoryRoute from "../modules/category/category.route.js";
+import genreRoute from "../modules/genre/genre.route.js";
 
 const appRouter = Router();
 
@@ -13,5 +15,7 @@ appRouter.use("/auth", authRoute);
 appRouter.use("/vendors", vendorRoute);
 appRouter.use("/permissions", permissionRoute);
 appRouter.use("/roles", roleRoute);
+appRouter.use("/categories", categoryRoute);
+appRouter.use("/genres", genreRoute);
 
 export default appRouter;
