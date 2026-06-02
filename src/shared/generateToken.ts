b5643c131afffb97a19b4fa4147d68d1c2347020
raaz-srijan <srijannospace@gmail.com>
2026-06-02 04:54:23 +0000

    destructured the payload

--- src/shared/generateToken.ts
+++ src/shared/generateToken.ts
@@ -1,10 +1,10 @@
 import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 import { ENV } from "../infrastructure/env";
 
-const JWT_SECRET = ENV.JWT_SECRET
-const JWT_REFRESH = ENV.JWT_REFRESH
-const JWT_SECRET_TIMEOUT = ENV.JWT_SECRET_TIMEOUT
-const JWT_REFRESH_TIMEOUT = ENV.JWT_REFRESH_TIMEOUT
+const JWT_SECRET = ENV.JWT_SECRET;
+const JWT_REFRESH = ENV.JWT_REFRESH;
+const JWT_SECRET_TIMEOUT = ENV.JWT_SECRET_TIMEOUT;
+const JWT_REFRESH_TIMEOUT = ENV.JWT_REFRESH_TIMEOUT;
 
 export interface PayloadTypes extends JwtPayload {
     id: string;
@@ -15,22 +15,25 @@ export interface PayloadTypes extends JwtPayload {
 
 //ACCESS TOKEN (SHORT_TIME)
 export function generateAccessToken(payload: PayloadTypes, expiresIn = JWT_SECRET_TIMEOUT as NonNullable<SignOptions["expiresIn"]>) {
-    return jwt.sign(payload, JWT_SECRET, { expiresIn });
+    const cleanPayload = { id: payload.id, email: payload.email, role: payload.role };
+    return jwt.sign(cleanPayload, JWT_SECRET, { expiresIn });
 }
+
 
 //REFRESH TOKEN (COOKIES)
 export function generateRefreshToken(payload: PayloadTypes, expiresIn = JWT_REFRESH_TIMEOUT as NonNullable<SignOptions["expiresIn"]>) {
-    return jwt.sign(payload, JWT_REFRESH, {expiresIn});
+    const cleanPayload = { id: payload.id, email: payload.email, role: payload.role };
+    return jwt.sign(cleanPayload, JWT_REFRESH, { expiresIn });
 }
 
 
 //VERIFY ACCESS TOKEN
-export function verifyAccessToken(accessToken:string):PayloadTypes {
+export function verifyAccessToken(accessToken: string): PayloadTypes {
     return jwt.verify(accessToken, JWT_SECRET) as PayloadTypes;
 }
 
 
 //VERIFY REFRESH TOKEN
-export function verifyRefreshToken(refreshToken:string):PayloadTypes {
+export function verifyRefreshToken(refreshToken: string): PayloadTypes {
     return jwt.verify(refreshToken, JWT_REFRESH) as PayloadTypes;
 }
