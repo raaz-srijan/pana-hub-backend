f54e3d2dbfe156c7a0041a6b0d52fac0532d87f4
raaz-srijan <srijannospace@gmail.com>
2026-06-03 04:40:26 +0000

    added a file validation

--- src/shared/middleware/upload.ts
+++ src/shared/middleware/upload.ts
@@ -31,10 +31,18 @@ const storage = multer.diskStorage({
     }
 });
 
+
 // Create the upload middleware instance
 export const upload = multer({ 
     storage: storage,
     limits: {
         fileSize: 5 * 1024 * 1024 
+    },
+     fileFilter: (req, file, cb) => {
+        // Accept images only
+        if (!file.mimetype.startsWith("image/")) {
+            return cb(new Error("Only image files are allowed!") as any, false);
+        }
+        cb(null, true);
     }
 });
