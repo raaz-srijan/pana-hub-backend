d9a5d17bf653bc7b59101cc24f3e2e00f6db553f
raaz-srijan <srijannospace@gmail.com>
2026-06-03 04:42:02 +0000

    added book module

---
+++ src/modules/book/book.route.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { BookController } from "./book.controller";
+import { auth } from "../../shared/middleware/auth";
+import { restrictTo } from "../../shared/middleware/rbac";
+import { upload } from "../../shared/middleware/upload";
+
+const router = Router();
+
+
+//PUBLIC ROUTES
+router.get("/search", BookController.searchBook); 
+router.get("/", BookController.getVerifiedBooks);
+router.get("/:id", BookController.getBookById);
+
+router.use(auth);
+
+//ADMIN ONLY ROUTES
+router.get("/admin/all", restrictTo("admin"), BookController.getAllBooks);
+router.get("/admin/unverified", restrictTo("admin"), BookController.getUnVerifiedBooks);
+router.get("/admin/trash", restrictTo("admin"), BookController.getTrashedBooks);
+
+router.patch("/:id/verify", restrictTo("admin"), BookController.toggleVerification);
+router.delete("/:id/permanent", restrictTo("admin"), BookController.permanentlyDeleteBook);
+
+
+// VENDOR (OR ADMIN) ROUTES
+router.post("/", restrictTo("vendors", "admin"), upload.single("image"), BookController.addBook);
+router.put("/:id", restrictTo("vendors", "admin"), upload.single("image"), BookController.updateBook);
+router.delete("/:id", restrictTo("vendors", "admin"), BookController.deleteBook); 
+
+export default router;
